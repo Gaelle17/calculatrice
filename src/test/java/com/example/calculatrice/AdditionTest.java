4328b4f42a7db0e8960c2f1f3cb39c524f124ee7
Gui <ethevegwenola@gmail.com>
2019-04-02 13:48:42 +0200

    Test addition -1 + 4

--- src/test/java/com/example/calculatrice/AdditionTest.java
+++ src/test/java/com/example/calculatrice/AdditionTest.java
@@ -7,8 +7,11 @@ import org.junit.Test;
 public class AdditionTest {
 	
 	@Test
-	public void additionEntier() {
+	public void additionTest1() {
 		assertTrue(2 + 4 == Addition.addition(2, 4));
 	}
-	
+	@Test
+	public void additionTest2() {
+		assertTrue(-1 + 4 == Addition.addition(-1, 4));
+	}
 }
