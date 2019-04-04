85a24bf4936cef30584e2c342987d698fc46172e
Gaëlle CARIO <gaellecario@MacBook-Pro-de-Gaelle.local>
2019-04-04 14:55:59 +0200

    soustraction test 5-5

--- src/test/java/com/example/calculatrice/SoustractionTest.java
+++ src/test/java/com/example/calculatrice/SoustractionTest.java
@@ -1,5 +1,19 @@
 package com.example.calculatrice;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
 public class SoustractionTest {
 
+  @Test
+  public void soustractionClassExistTest() {
+    Class<Soustraction> klass = Addition.class;
+    assertTrue("com.example.calculatrice.Soustraction".equals(klass.getName()));
+  }
+
+  @Test
+  public void soustractionTest0() {
+    assertTrue(5 - 5 == Soustraction.soustraction(5, 5));
+  }
 }
