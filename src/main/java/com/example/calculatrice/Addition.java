5a42416a4d058c91a90cc06e48544f9ffdfb83a3
Gui <ethevegwenola@gmail.com>
2019-04-04 14:25:54 +0200

    checkstyle main ok

--- src/main/java/com/example/calculatrice/Addition.java
+++ src/main/java/com/example/calculatrice/Addition.java
@@ -2,15 +2,10 @@ package com.example.calculatrice;
 
 public class Addition {
 
-	public static Integer addition(Integer a, Integer b){
-		return a + b;
-	}
+  public static Integer addition(Integer a, Integer b) {
+    return a + b;
+  }
 
-	public Addition() {
-		
-	}
-
-	
-	
-	
+  public Addition() {
+  }
 }
